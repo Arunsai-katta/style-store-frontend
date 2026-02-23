6e0d7629b5b510b34c91ba981c111c0cbeb73d78
Arun Sai <arunsai@labsquire.com>
2026-02-24 01:21:31 +0530

    update next.config.js to dynamically set image domains and add vercel.json configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
+const imageDomains = ['localhost'];
+
+if (process.env.NEXT_PUBLIC_S3_DOMAIN) {
+  imageDomains.push(process.env.NEXT_PUBLIC_S3_DOMAIN.replace(/^https?:\/\//, ''));
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   images: {
-    domains: [
-      'localhost',
-      process.env.NEXT_PUBLIC_S3_DOMAIN || '*.amazonaws.com'
-    ],
+    domains: imageDomains,
     remotePatterns: [
       {
         protocol: 'https',
